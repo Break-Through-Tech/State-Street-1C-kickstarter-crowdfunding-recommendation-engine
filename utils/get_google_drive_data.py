088ab125304b4f0e839f32dda9fbbd94ca5f126e
annasehgal <annasehgal05@gmail.com>
2026-09-15 11:34:32 -0700

    Fix: only keep local drive path to download data

--- utils/get_google_drive_data.py
+++ utils/get_google_drive_data.py
@@ -1,12 +1,12 @@
 import os
-import io
+#import io
 from pathlib import Path
-import codecs
+#import codecs
 from dotenv import load_dotenv
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 import pandas as pd
-from utils.google_drive_client import get_drive_client
+from google_drive_client import get_drive_client
 
 load_dotenv()
 
@@ -55,28 +55,28 @@ def download_csv_data(service, local_path):
         while not done:
             _, done = downloader.next_chunk()
 
-def _cp1252_fallback(err):
-    bad_bytes = err.object[err.start:err.end]
-    return bad_bytes.decode("cp1252", errors="replace"), err.end
+#def _cp1252_fallback(err):
+#    bad_bytes = err.object[err.start:err.end]
+#    return bad_bytes.decode("cp1252", errors="replace"), err.end
 
-codecs.register_error("cp1252_fallback", _cp1252_fallback)
+#codecs.register_error("cp1252_fallback", _cp1252_fallback)
 
-def load_csv_from_drive(service):
-    data_file = get_datafile(service)
+#def load_csv_from_drive(service):
+ #   data_file = get_datafile(service)
 
-    if not data_file["capabilities"]["canDownload"]:
-        raise PermissionError(f"File cannot be downloaded: {data_file['name']}")
+  #  if not data_file["capabilities"]["canDownload"]:
+   #     raise PermissionError(f"File cannot be downloaded: {data_file['name']}")
 
-    request = service.files().get_media(fileId=data_file["id"])
-    buffer = io.BytesIO()
-    downloader = MediaIoBaseDownload(buffer, request)
+    #request = service.files().get_media(fileId=data_file["id"])
+    #buffer = io.BytesIO()
+    #downloader = MediaIoBaseDownload(buffer, request)
 
-    done = False
-    while not done:
-        _, done = downloader.next_chunk()
+    #done = False
+    #while not done:
+     #   _, done = downloader.next_chunk()
 
-    text = buffer.getvalue().decode("utf-8", errors="cp1252_fallback")
-    return pd.read_csv(io.StringIO(text))
+    #text = buffer.getvalue().decode("utf-8", errors="cp1252_fallback")
+    #return pd.read_csv(io.StringIO(text))
 
 def load_csv_data(local_path: str) -> pd.DataFrame:
     return pd.read_csv(local_path)
